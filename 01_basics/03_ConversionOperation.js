0641a68be84f777990617d7f486dec3110276087
Satvik Mishra <105464790+Isatvikmishra@users.noreply.github.com>
2023-08-27 19:00:23 +0000

    Operation

--- 01_basics/03_ConversionOperation.js
+++ 01_basics/03_ConversionOperation.js
@@ -24,3 +24,62 @@ let someNumber = 22
 let StringNumber = String(someNumber)
 console.log(StringNumber);
 console.log(typeof StringNumber);
+
+
+
+// ***************************OPERATIONS*************************************
+
+let value = 3
+let negvalue = -value
+
+/*
+console.log(negvalue);
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2);
+console.log(2/2);
+console.log(2%3);
+*/
+
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);  ** If string is in start then everything is treated as string.
+// console.log(1 + "2");  **If string is in last then first it will perform operation then.
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2"); *These are good for questions in exam but is bad practice.
+
+// console.log( (3 + 4) * 5 % 3); *Use this insttead 
+
+// console.log(+true);
+// console.log(+"");
+ 
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);  
+
+/*
+# If used postfix, with operator after operand (for example, x++), the increment operator increments and returns the value before incrementing.
+
+# If used prefix, with operator before operand (for example, ++x), the increment operator increments and returns the value after incrementing.
+
+*/
+
+
+let x = 3
+const y = x++
+console.log(y,x);  // x is 4; y is 3
+
+let a = 3
+const b = ++a
+console.log(b,a); // x is 4; y is 4
+
+
+
+

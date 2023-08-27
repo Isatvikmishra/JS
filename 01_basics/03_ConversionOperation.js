abdc6abecfa3b21ac0d37c0af7cd21a38dc607c2
Satvik Mishra <105464790+Isatvikmishra@users.noreply.github.com>
2023-08-27 18:35:35 +0000

    Conversion of data types

---
+++ 01_basics/03_ConversionOperation.js
@@ -0,0 +1,26 @@
+let score = "Satvik"
+//console.log(typeof score);
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let IsLoggedIn = 1
+
+let BooleanIsLoggedIn = Boolean(IsLoggedIn)
+console.log(BooleanIsLoggedIn);
+
+// 1 => true
+// 0 => false
+// "Satvik" => true
+
+
+let someNumber = 22
+
+let StringNumber = String(someNumber)
+console.log(StringNumber);
+console.log(typeof StringNumber);

9b34f9325a9a94908b222c70be7bcc574463b63d
Satvik Mishra <105464790+Isatvikmishra@users.noreply.github.com>
2023-08-27 17:29:46 +0000

    Datatypes

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -1,1 +1,30 @@
 "use strict" // Treat as newer version of js code
+
+// alert (3+3) //We are using nodejs not browser
+
+
+let name = "Satvik"
+let age = 18
+let isLoggedIn = false
+
+
+
+//DATA TYPES
+
+/*
+Number => 2 to power 53
+bigint => Bigger data
+string => ""
+boolean => True/False
+null => Standalone value
+undefined => Not defined (let state;)
+symbol => unique
+object
+*/
+
+console.log(typeof undefined)    //Output is Undefined
+
+// typeof tells the type of value/datatype
+
+console.log(typeof null)    //Output is object
+

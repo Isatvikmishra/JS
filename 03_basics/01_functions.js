4ca1238347862f705aa7fa3f087aac7e1384523b
Satvik Mishra <105464790+Isatvikmishra@users.noreply.github.com>
2023-09-30 18:26:44 +0000

    function 1

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -54,5 +54,13 @@ console.log(userLoggedIn("SAM")); // it will nver get undefined as we defined it
 function calculateCartPrice(val1,val2, ...num1){
 	return num1
 }
-console.log(calculateCartPrice(200,400,500,2000)); //Here 200,400 stored in val1,val2 and ... is rest and spread operator is used to make bundle of things.
+// console.log(calculateCartPrice(200,400,500,2000)); # Here 200,400 stored in val1,val2 and ... is rest and spread operator is used to make bundle of things.
 
+const user = {
+  username: "MOUSE",
+  price: 199
+}
+
+function handleObject(anyobject){
+  console.log(`Username is ${anyobject.username}and price is ${anyobject.price}`);
+}

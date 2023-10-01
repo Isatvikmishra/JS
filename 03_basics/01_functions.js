7a67ad9b4d1b6273771a8c4832d78688adc9d9cb
Satvik Mishra <105464790+Isatvikmishra@users.noreply.github.com>
2023-10-01 15:47:16 +0000

    SCOPES

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -56,6 +56,8 @@ function calculateCartPrice(val1,val2, ...num1){
 }
 // console.log(calculateCartPrice(200,400,500,2000)); # Here 200,400 stored in val1,val2 and ... is rest and spread operator is used to make bundle of things.
 
+//Object 
+
 const user = {
   username: "MOUSE",
   price: 199
@@ -64,3 +66,13 @@ const user = {
 function handleObject(anyobject){
   console.log(`Username is ${anyobject.username}and price is ${anyobject.price}`);
 }
+
+//Array
+const myNewArray = [200, 400, 500, 800]
+
+function returnSecondValue(getArray){
+  return getArray[1]
+}
+
+console.log(returnSecondValue(myNewArray));
+
---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,55 @@
+var c = 50
+let z = 80
+let a= 300
+
+console.log(a);
+if (true) {
+    let a = 10
+    console.log("Inner: ", a);
+
+    const b = 20
+    var c = 30
+    console.log(z); //value of Global scope is available inside the block scope.
+}
+
+// console.log(a); 
+// ReferenceError: a is not defined. As let is inside block scope and value of block scope is not available for global scope. 
+
+// console.log(b);
+// ReferenceError: b is not defined. As const is inside block scope and value of block scope is not available for Global
+
+console.log(c); //Here we get the value of c as this is problem of var even if we decalare another var with variable c it will still fetch value 30. So we avoid var.
+
+
+
+
+
+//// NESTED FUNCTION 
+
+function one() {
+    const username = "Satvik"
+
+    function two(){
+        const website = "Git"
+        console.log(username);
+    }
+    // console.log(website); As we are accessing it outside the block scope.
+    two() // This got executed as we accessed global scope username in block scope.
+}
+one()
+
+
+///INTERESTING///
+
+console.log(addOne(5));
+function addOne(num){
+    return num + 1
+}
+// Using this we can access before initialization
+
+console.log(addTwo(2));
+const addTwo = function(num){
+    return num + 2
+}
+
+// We can access Using this before initialization

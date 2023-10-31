49b44751fac0229f050c9c287a25e5ce5051c893
Satvik Mishra <105464790+Isatvikmishra@users.noreply.github.com>
2023-10-31 19:51:20 +0000

    reduce, filter, map, for in, each, of

---
+++ 05_iterations/7.js
@@ -0,0 +1,12 @@
+const myNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// const newNums = myNumers.map( (num) => { return num + 10})
+
+
+//Chaining
+const newNums = myNumbers
+                .map((num) => num * 10 ) //After applying this ot will pass value to next 
+                .map( (num) => num + 1)
+                .filter( (num) => num >= 40)
+
+console.log(newNums);
